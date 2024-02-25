25d118008b038c7a6549ca9948bd34a01ccc7017
HienPham <pvhien.ehs.2020@gmail.com>
2024-02-25 21:41:46 +0700

    create Project_022524

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const express =require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//handlebars
+const engineHandleBars  = require ('express-handlebars');
+
+//static folder
+app.use(express.static(__dirname + '/securityPuplic'));
+
+
+
+//define engine handlebars
+app.engine('hbs',engineHandleBars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout'
+}));
+app.set('view engine','hbs');
+
+
+
+//route
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.get('/:page',(req,res)=>{
+    res.render(req.params.page);
+});
+
+app.get('/',(req,res)=>{
+    res.send('Hello world');
+});
+//start web server;
+app.listen(port,()=>{
+    console.log(`server is listening on port ${port}`);
+})
